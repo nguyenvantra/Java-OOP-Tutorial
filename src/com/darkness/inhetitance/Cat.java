1ec629a345423b4f162706b3cfdf69d11ceed034
nguyenvantra <dkdarknessnvt@gmail.com>
2017-07-24 16:40:55 +0700

    add inheritance

---
+++ src/com/darkness/inhetitance/Cat.java
@@ -0,0 +1,9 @@
+package com.darkness.inhetitance;
+
+public class Cat extends Animal {
+
+	public Cat(String color, int age, int weight) {
+		super(color, age, weight);
+	}
+
+}
---
+++ src/com/darkness/inhetitance/Dog.java
@@ -0,0 +1,9 @@
+package com.darkness.inhetitance;
+
+public class Dog extends Animal {
+
+	public Dog(String color, int age, int weight) {
+		super(color, age, weight);
+	}
+
+}
