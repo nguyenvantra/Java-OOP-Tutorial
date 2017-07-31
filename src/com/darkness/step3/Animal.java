c4648d7a8ea03b7e6342fdd1360ede4f3f3dcaf8
nguyenvantra <dkdarknessnvt@gmail.com>
2017-08-01 01:20:43 +0700

    add src inheritance in java

---
+++ src/com/darkness/step3/Animal.java
@@ -0,0 +1,26 @@
+package com.darkness.step3;
+
+public class Animal {
+	private String color;
+	private int age;
+	private int weight;
+
+	public Animal(String color, int age, int weight) {
+		this.color = color;
+		this.age = age;
+		this.weight = weight;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+}
---
+++ src/com/darkness/step3/Dog.java
@@ -0,0 +1,9 @@
+package com.darkness.step3;
+
+public class Dog extends Animal {
+
+	public Dog(String color, int age, int weight) {
+		super(color, age, weight);
+	}
+
+}
